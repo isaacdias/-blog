3fb05d10647d67841ba915e575e2f6dd8699986e
isaacdias <isaacrdias@gmail.com>
2019-09-22 11:39:39 -0300

    adicionando imagem no card

--- myproject/blog/forms.py
+++ myproject/blog/forms.py
@@ -5,4 +5,4 @@ from .models import Post
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'slug', 'body', 'author', 'status']
+        fields = ['title', 'slug', 'image', 'body', 'author', 'status']
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -121,5 +121,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, "static/")
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
