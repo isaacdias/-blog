7e995a10967d01661e1c96da1e1b18f938a6d489
isaacdias <isaacrdias@gmail.com>
2019-09-23 21:30:02 -0300

    alteração da conficuração de media

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -123,13 +123,12 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, "static/")
+     os.path.join(BASE_DIR, "static")
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+MEDIA_ROOT = "{}/media".format(BASE_DIR)
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
