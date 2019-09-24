9d665a9de784e2e4a86eea5eb3eef3badf0a4ef9
isaacdias <isaacrdias@gmail.com>
2019-09-24 14:32:01 -0300

    inclusão do form=forms.ImageFiel()

--- myproject/blog/forms.py
+++ myproject/blog/forms.py
@@ -1,8 +1,9 @@
-
+from django import forms
 from django.forms import ModelForm
 from .models import Post
 
 class PostForm(ModelForm):
+    image = forms.ImageField()
     class Meta:
         model = Post
         fields = ['title', 'slug', 'image', 'body', 'author', 'status']
