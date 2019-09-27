4230bc3cb01df1d0602b5dfd2d2e9bf61421aaea
isaacdias <isaacrdias@gmail.com>
2019-09-27 11:56:14 -0300

    refatoração das views post_ create e post_update

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -5,67 +5,64 @@ from django.core.paginator import Paginator
 from .models import Post
 from .forms import PostForm
 
-def post_list(request):
-	post_list = Post.objects.all().order_by('title')
-	paginator = Paginator(post_list, 9)
 
-	page = request.GET.get('page')
-	posts = paginator.get_page(page)
-	return render(request, 'blog/post_list.html', {'posts':posts})
+def post_list(request):
+    post_list = Post.objects.all().order_by('title')
+    paginator = Paginator(post_list, 9)
+
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, slug):
-	post = get_object_or_404(Post, slug=slug)
-	return render(request, 'blog/post_detail.html', {'post':post})
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 
 @login_required
 def post_create(request):
-	form = PostForm()
+    form = PostForm()
 
-	if request.method == 'POST':
+    if request.method == 'POST':
 
-		form = PostForm(request.POST, request.FILES)
+        form = PostForm(request.POST, request.FILES)
 
-		if form.is_valid():
-			form.save()
+        if form.is_valid():
+            form.save()
 
-			return redirect('blog:post_list')
+            return redirect('blog:post_list')
+        
+        else:
+            return render(request, 'blog/add_post.html', {'form': form})
 
-	elif request.method == 'GET':
-		return render(request, 'blog/add_post.html', {'form':form})
+    elif request.method == 'GET':
+        return render(request, 'blog/add_post.html', {'form': form})
+
 
 @login_required
 def post_update(request, id):
     post = get_object_or_404(Post, pk=id)
-	form = PostForm(instance=post)
-    
-    if request.method == 'POST':
-        
+    form = PostForm(instance=post)
+
+    if(request.method == 'POST'):
         form = PostForm(request.POST, request.FILES, instance=post)
-        
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.title = form.cleaned_data['title']
-            post.slug = form.cleaned_data['slug']
-            post.body = form.cleaned_data['body']
-            post.image = form.cleaned_data['image']
-            post.author = form.cleaned_data['author']
-            post.status = form.cleaned_data['status']
+
+        if(form.is_valid()):
+
             post.save()
-            
+
             return redirect('blog:post_list')
-        
         else:
-            return render(request, 'blog/edit_post.html', {'form':form, 'post':post})
-    
-    elif request.method == 'GET':
-        return render(request, 'blog/edit_post.html', {'form':form, 'post':form})
-            
-                        
-    
-    
+            return render(request, 'blog/edit_post.html', {'form': form, 'post': post})
+
+    elif(request.method == 'GET'):
+        return render(request, 'blog/edit_post.html', {'form': form, 'post': post})
+
+
 def about(request):
-	return render(request, 'blog/about_us.html')
+    return render(request, 'blog/about_us.html')
+
 
 def contact(request):
-	return render(request, 'blog/contact.html')
+    return render(request, 'blog/contact.html')
