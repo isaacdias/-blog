fc542c09aaeff673f96621c36e325650e3c6f555
isaacdias <isaacrdias@gmail.com>
2019-09-27 14:27:59 -0300

    add view post_delete

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -59,6 +59,12 @@ def post_update(request, id):
     elif(request.method == 'GET'):
         return render(request, 'blog/edit_post.html', {'form': form, 'post': post})
 
+@login_required
+def post_delete(request, id):
+    post = get_object_or_404(Post, pk=id)
+    post.delete()
+    return redirect('blog:post_list')
+
 
 def about(request):
     return render(request, 'blog/about_us.html')
