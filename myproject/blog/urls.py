853f6f913571b79ebbf7378dd5ad42dabdfc0cbf
isaacdias <isaacrdias@gmail.com>
2019-09-27 14:27:17 -0300

    criação da url deletar/<int:id>

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('<slug:slug>', views.post_detail, name='post_detail'),
     path('criar/', views.post_create, name='post_create'),
     path('editar/<int:id>/', views.post_update, name='post_update'),
+    path('deletar/<int:id>/', views.post_delete, name='post_delete'),
     path('sobre-nos/', views.about, name='about'),
     path('contato/', views.contact, name='contact'),
 ]
