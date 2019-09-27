7c4683c8a7719e63328c39863de8b377bda6adfc
isaacdias <isaacrdias@gmail.com>
2019-09-27 09:57:10 -0300

    criaçao da def post_update

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -34,7 +34,36 @@ def post_create(request):
 	elif request.method == 'GET':
 		return render(request, 'blog/add_post.html', {'form':form})
 
-
+@login_required
+def post_update(request, id):
+    post = get_object_or_404(Post, pk=id)
+	form = PostForm(instance=post)
+    
+    if request.method == 'POST':
+        
+        form = PostForm(request.POST, instance=post)
+        
+        if form.is_valid:
+            post = form.save(commit=False)
+            post.title = form.cleaned_data['title']
+            post.slug = form.cleaned_data['slug']
+            post.body = form.cleaned_data['body']
+            post.image = form.cleaned_data['image']
+            post.author = form.cleaned_data['author']
+            post.status = form.cleaned_data['status']
+            post.save()
+            
+            return redirect('blog:post_list')
+        
+        else:
+            return render(request, 'blog/edit_post.html', {'form':form, 'post':form})
+    
+    elif request.method == 'GET':
+        return render(request, 'blog/edit_post.html', {'form':form, 'post':form})
+            
+                        
+    
+    
 def about(request):
 	return render(request, 'blog/about_us.html')
 
