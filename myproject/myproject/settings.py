31823b9534f3c179a84fd3dd2c94599c594c244c
isaacdias <isaacrdias@gmail.com>
2019-09-28 16:02:59 -0300

    configurando o email para page de contato

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -72,6 +72,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
+# email backend
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'blogdjangoframework@gmail.com' 
+EMAIL_HOST_PASSWORD = 'testeblogdjango'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
