4f1b42d9e485f4ee4e6177946e26a6ac658eb676
isaacdias <isaacrdias@gmail.com>
2019-11-29 21:53:27 -0300

    inclusão db

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'blogdjangoframework@gmail.com' 
-EMAIL_HOST_PASSWORD = '***************'
+EMAIL_HOST_PASSWORD = 'testeblogdjango'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
